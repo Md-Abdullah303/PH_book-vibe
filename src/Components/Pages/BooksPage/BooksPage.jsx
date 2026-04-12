9c3504367b81edf595d8cebdad2f983723ff7c87
Mohammad Abdullah <mdabdulla01715940008@gmail.com>
2026-04-12 13:06:31 +0600

    tailwind,daisiUI,react router install done, and routing test in some page

---
+++ src/Components/Pages/BooksPage/BooksPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BooksPage = () => {
+    return (
+        <div>
+            <p>books page</p>
+        </div>
+    );
+};
+
+export default BooksPage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Layout from './layout/Layout'
+import HomePage from './Components/Pages/HomePage/HomePage'
+import BooksPage from './Components/Pages/BooksPage/BooksPage'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: Layout,
+    children: [
+      {index: true, Component: HomePage},
+      {path: '/books', Component: BooksPage},
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
